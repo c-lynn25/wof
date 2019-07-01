e7684399a3688f3fe09dc9a2c8541b6deb6330c7
Student <Student@FPHACK9.mgsops.net>
2019-07-01 14:30:05 +0200

    new updates

--- spin.js
+++ spin.js
@@ -1,25 +1,7 @@
-console.clear();
+var box = document.getElementById("box")
+var tl = new TimelineLite({ delay: 1 });
 
-//it doesn't get much easier than this;)
-Draggable.create("#knob", {
-    type: "rotation",
-    throwProps: true,
-    onDrag: function () {
-        console.log(this.rotation)
-    }
-});
-
-$("#rotation").click(function () {
-    console.log(document.getElementById("knob")._gsTransform.rotation, "from element");
-    console.log(Draggable.get("#knob").rotation, "from Draggable.get()");
-});
-
-/* note this file loads
-
-TweenMax.min.js
-Draggable.min.js
-ThrowPropsPlugin.min.js (Club GreenSock bonus plugin for velocity-based tweens)
-
-More info on Club GreenSock and other bonus plugins
-sss
-*/
+tl.to(box, 0.25, { rotation: 20 })
+    .to(box, 0.5, { rotation: -20, ease: Power1.easeInOut, repeat: 3, yoyo: true, repeatDelay: 0.5 }, 0.5)
+    //end at 0
+    .to(box, 0.25, { rotation: 0 }, "+=0.5")
