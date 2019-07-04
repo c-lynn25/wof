c82ad2583082cd9fa2efe6c50de4418761926950
007BlackGirl <ayandambuthu@yahoo.com>
2019-07-04 09:25:11 +0200

    javascript for login,forgot and register pages

---
+++ loginFunction.js
@@ -0,0 +1,30 @@
+var email = "chanmunien@gmail.com", password = "PASSWORD";
+
+function validate() {
+    var emailcheck = document.getElementById("email").value;
+    var passwordcheck = document.getElementById("password").value;
+    if (passwordcheck == password && emailcheck == email) {
+        //welcome user by username, access db for this
+        alert("Welcome user.");
+    }
+    else {
+        alert("Please recheck your email and password combination!")
+    }
+} 
+
+function resetPassword() {
+    var toReset = document.getElementById("resetThis").value;
+    alert("A link has been sent to " + toReset+" to reset your password.");
+}
+
+
+function validateSignUp() {
+    var user = document.getElementById("user").value;
+    var useremail = document.getElementById("signUpEmail").value;
+    var userPassword = document.getElementById("signUpPassword").value;
+    var userConfirm = document.getElementById("signUpPasswordConfirm").value;
+
+    if (userPassword != userConfirm) {
+        alert("Your passwords do not match!");
+    }
+}
