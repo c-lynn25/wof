a4789ace389e7494d188bdc5ec0e5ee98086875a
c-lynn25 <52172476+c-lynn25@users.noreply.github.com>
2019-07-04 17:36:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,101 @@
-// OM animation with Greensock  ---------------------------------
+(($) => {
+    var wheel = $('#wheel');
+    var bet = $('#betAmount');
+    var arrow = $('#arrow');
+    var info = $("#info");
+    var credit=100;
+    var prize = 0;
 
-$(document).ready(function () {
-    //  Setup variables
-    var wheel = $(".wheel"),
-        active = $(".active");
-
-    $btnPlay = $("#btnPlay"),
-        $btnSlowMo = $("#btnSlowMo");
-
-    //  Random degree
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    var deg = getRandomInt(720, 2400);
-    var repeatN = Math.floor(deg / 360);
-    console.log(deg, repeatN);
-
-    //  Creating the Timeline
-    var indicator = new TimelineMax({});
-    var spinWheel = new TimelineMax();
-    indicator.to(active, 0.13, { rotation: -10, transformOrigin: "65% 36%", ease: Power1.easeOut })
-        .to(active, 0.13, { rotation: 3, backgroundColor: 'red', transformOrigin: "65% 36%", ease: Power1.easeOut })
-
-
-    //  Indicator animation
-    // for(var i=0; i < repeatN; i++){
-    //   indicator.time(i);
-    // }
-
-
-    //  Luckywheel animation
-    spinWheel.to(wheel, 5, {
-        rotation: deg, transformOrigin: "50% 50%", ease: Power4.easeOut, onUpdate: (
-            function () {
-                var currentRotation = this.target[0]._gsTransform.rotation;
-                console.log(currentRotation);
-                if (Math.round(currentRotation) % (360 / 12) <= 2) {
-                    if (indicator.progress() > 0.1 || indicator.progress() === 0) {
-                        indicator.play(0);
-                    }
-                    console.log(indicator.progress());
-                }
-            }
-        ), onComplete: (
-
-            function () {
-
-                TweenLite.defaultEase = Linear.easeNone;
-
-                var bounds = $(".wheel")[0].getBBox();
-                var blast = $("#blast");
-
-                var center = {
-                    x: bounds.x + bounds.width / 2,
-                    y: bounds.y + bounds.height / 2,
-                };
-
-                var stagger = 1;
-                var radius = getDistance(center, bounds);
-
-                TweenLite.set(blast, { attr: { r: radius, cx: center.x, cy: center.y } });
-
-                TweenLite.set(blast, { transformOrigin: "center", scale: 0 });
-
-                var tl = new TimelineMax()
-                    .to(blast, stagger, { scale: 1 }, 0)
-                    .to(blast, stagger, { scale: 2, autoAlpha: 0 }, stagger)
-
-                TweenMax.to(tl, 5, {
-                    progress: 1,
-                    ease: Expo.easeInOut,
-                    repeat: -1,
-                });
-
-                function getDistance(p1, p2) {
-                    var dx = p2.x - p1.x;
-                    var dy = p2.y - p1.y;
-                    return Math.sqrt(dx * dx + dy * dy);
-                }
-
-            }
-        )
+    var sound = new Howl({
+      src: ['https://www.wheeloffortune.com/Content/Assets//so-many-ways-to-play/downloads/wheel_spin.mp3'],
+      volume: 0.5
+    });
+    
+    var numItems = 12;
+    var arc = 360 / numItems; // 30
+    
+    TweenLite.set(wheel, { rotation: 360 });
+    
+    var prizes = [
+      900,
+      -8,
+      7,
+      -6,
+      5,
+      -4,
+      3,
+      -2,
+      1,
+      -12,
+      11,
+      -10
+    ];
+    
+    arrow.on('click', () => {
+  
+      sound.play();
+      
+      // var random = Math.floor(Math.random() * 8000) + 6000;
+       var random = Math.floor(Math.random() * 800) + 600;
+       var tl = new TimelineMax();
+       tl.to(wheel, 7, { rotation:  "+=" + random, transformOrigin: "50% 50%", ease: Back.easeOut.config(1) })
+  
+       TweenLite.to(tl, 4, {timeScale:0, ease: Power1.easeOut, delay: 3, onComplete: puaseAud })
+       
     })
-        .add("end");
+    
+    TweenLite.ticker.addEventListener("tick", update);
+    
+    function puaseAud(){
+      sound.stop(0); 
+      
+      //Zoom out results
+      TweenMax.to(info, 1, {scale:1.5, ease:Bounce.easeOut})
+      TweenMax.to(info, 0.2, {scale:1, delay:0.4})
 
-    //   Buttons
-    $btnPlay.click(
-        function () {
-            spinWheel.timeScale(1).seek(0);
-            indicator.timeScale(1).seek(0);
+      if(prize > 0)
+      info.html( prize +" Credits Won!!");
+      else
+      info.html( "Sorry, you lost "+ bet +" Credits.");
+      
+      addCredit(prize)
+  
+    }
+  
+    function update() {
+    console.log(wheel)
+    console.log(wheel[0])  
+      var elem = wheel[0]._gsTransform.rotation;     // 60
+    
+      var angle = elem - (360 * Math.floor(elem / 360));     // 60 
+      var index = Math.floor((360 - angle) / arc) % prizes.length; // (300 /30) % 12 = 0  
+      prize = prizes[index];
+      
+      
+      //info.html( " current prize = " + prize +" ,index = "+index);
+    }
+       
+    //pauseTween && pauseTween.kill();
+  
+    function addCredit(x){
+        credit += x;
+        displayCredits();
+    }
+    function removeCredit(x){
+        if(credit>=x){
+            credit -= x;
+            displayCredits();
         }
-    );
-
-    $btnSlowMo.click(
-        function () {
-            indicator.timeScale(0.2).seek(0.5);
-            spinWheel.timeScale(0.2).seek(0.5);
+        else{
+            alert("End of game");//Pop up saying that the player has lost or to restart
+            displayCredits();
         }
-    );
-
-
-});
+    }
+     
+    function displayCredits() {
+        document.getElementById("credAmount").innerHTML = credit;
+    }
+  
+  })(jQuery)
+  
+  
